19c9b6cf77959b9f610ac62db442c0eb8d8743e3
loc-admin <you@example.com>
2023-02-01 15:14:13 +0300

    Example4

---
+++ example4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число!");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int Num1 = 1;Num1 <= num; Num1++)
+if (Num1 % 2 == 0)
+System.Console.Write( Num1 + " ");
+
