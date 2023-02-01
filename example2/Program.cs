5274f5560662b44defc45e00f0241289d3024338
loc-admin <you@example.com>
2023-02-01 15:13:29 +0300

    Example2

---
+++ example2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число!");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Max ");
+Console.WriteLine(Math.Max(num1, Math.Max(num2, num3)));
