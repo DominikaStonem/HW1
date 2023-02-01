3789315b3f6939acd87c3a205357a695f9a4aa6a
loc-admin <you@example.com>
2023-02-01 15:12:36 +0300

    Example1

--- example1/Program.cs
+++ example1/Program.cs
@@ -9,12 +9,23 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2)
 {
-    System.Console.WriteLine("Max");
-    System.Console.Write(num1);
+    System.Console.WriteLine("Max ");
+    System.Console.WriteLine(num1);
 }
-else 
+else if (num1 < num2 )
 {
-    System.Console.WriteLine("Min");
-    System.Console.Write(num2);
+    System.Console.WriteLine("Max ");
+    System.Console.WriteLine(num2);
+
+}
+if (num1 > num2)
+{
+    System.Console.WriteLine("Min ");
+    System.Console.WriteLine(num2);
+}
+else if (num1 < num2 )
+{
+    System.Console.WriteLine("Min ");
+    System.Console.WriteLine(num1);
 
 }
